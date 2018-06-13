aff2b13b03c1a03a0c0f26a65cd00a09753323a4
Mihai Dumitru <mihai.dumitru2201@stud.acs.upb.ro>
2018-06-13 18:08:03 +0300

    initial commit

---
+++ fancy-ctrl-z.zsh
@@ -0,0 +1,11 @@
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
